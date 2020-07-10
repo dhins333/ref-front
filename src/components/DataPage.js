cca268b0cc3b2a376d4fd78cfb13e21e19c2075b
Dhinesh Kumar <dhins333@gmail.com>
2020-07-10 12:48:11 +0530

    python support

--- src/components/DataPage.js
+++ src/components/DataPage.js
@@ -5,25 +5,31 @@ import 'codemirror';
 import {Controlled as CodeMirror} from 'react-codemirror2';
 import axios from 'axios';
 require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/python/python');
 
 
 const DataPage = (props) => {
     const [value,setValue] = useState('');
+    const [lang,setLang] = useState('javascript');
+
+    const options = {
+        mode: lang,
+        theme: 'material',
+        lineNumbers: true
+      }
     
     useEffect(() => {
         const effectFunc = async () => {
             const dataResp = await axios.get(`/api/files/${props.match.params.id}/data`);
             const data = dataResp.data.data;
             setValue(data);
+            console.log(dataResp.data.lang);
+            setLang(dataResp.data.lang);
         }
         effectFunc();
     },[])
 
-    const options = {
-        mode: 'javascript',
-        theme: 'material',
-        lineNumbers: true
-      }
+
 
     return(
         <div>
